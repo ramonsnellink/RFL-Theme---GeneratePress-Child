303d452ca5a72d2863364ff0bcdf5dafa2493cad
Ramon Snellink <r.snellink@gmail.com>
2021-02-25 16:14:52 +0100

    added another button style, added a new gallery style

--- inc/block-styles.php
+++ inc/block-styles.php
@@ -17,4 +17,18 @@ add_action('init', function() {
 	]);
 });
 
+add_action('init', function() {
+	register_block_style('core/button', [
+		'name' => 'button-simple',
+		'label' => __('Simple Button - Without background', 'generatepress'),
+	]);
+});
+
+add_action('init', function() {
+	register_block_style('core/gallery', [
+		'name' => 'gallery-1lg-2sm',
+		'label' => __('Gallery with 1 large image and 2 to the side', 'generatepress'),
+	]);
+});
+
 
